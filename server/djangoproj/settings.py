56ab29dbbe3287ac2f0ee3f508a838d7669c3e04
Dmytro Klymenko <119627462+leegosx@users.noreply.github.com>
2024-06-28 01:45:54 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -123,8 +123,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -137,7 +135,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
